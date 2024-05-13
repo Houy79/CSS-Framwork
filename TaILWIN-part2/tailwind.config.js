8e33cf891fb6376a797afcf27c971fc1647ee04d
Houy79 <houylong70@gmial.com>
2024-05-13 15:59:26 +0700

    commit Css-framwork

---
+++ TaILWIN-part2/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      colors: {
+        // ...
+        primary: {
+          light: '#67e8f9',
+          DEFAULT: '#06b6d4',
+          dark: '#f97316',
+        },
+        
+      }
+    },
+  }
